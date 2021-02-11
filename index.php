dc404a6be0f5a1b452e4c9fd7f9f5072810ef693
ladiwals <vladjeslav@gmail.com>
2021-02-11 20:57:58 +0300

    Добавил фавиконку и файл стилей, начал список мини-проектов

--- index.php
+++ index.php
@@ -4,8 +4,15 @@
 <!DOCTYPE html>
 
 <head>
+	<meta charset="utf-8">
+	<title>PHP</title>
+	<link rel="SHORTCUT ICON" href="favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
 	<h1>Здесь будет всякая белиберда на основе PHP</h1>
+	<ul>
+		<li><a href="#">Качественная авторизация</a></li>
+	</ul>
 </body>
