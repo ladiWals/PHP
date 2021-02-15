ebb7fd270887d8ff3dd3b2d4a7fdf1c9b40e9f1a
ladiwals <vladjeslav@gmail.com>
2021-02-15 17:30:59 +0300

    Всё-таки добавил индексную страницу для кулавторити и в нём вставил ссылки на регистрацию, вход и главную

---
+++ coolAuth/index.php
@@ -0,0 +1,19 @@
+<?php
+?>
+
+<!DOCTYPE html>
+
+<head>
+	<meta charset="utf-8">
+	<title>PHP</title>
+	<link rel="stylesheet" href="/styles.css">
+</head>
+
+<body>
+	<h1></h1>
+	<ul>
+		<li><a href="/coolAuth/login.php">Регистрация</a></li>
+		<li><a href="/coolAuth/register.php">Вход</a></li>
+		<li><a href="/">Главная</a></li>
+	</ul>
+</body>
