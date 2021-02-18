b3451280b742a48eb2920dbceff5702a9a3746a8
ladiwals <vladjeslav@gmail.com>
2021-02-18 12:03:15 +0300

    добавил основыне поля расчёта самой ипотеки

--- profitHata/index.php
+++ profitHata/index.php
@@ -11,4 +11,18 @@
 
 <body>
 	<h1>Заполните все данные и нажмите "рассчитать"</h1>
+
+	<div class="mainForm">
+		<form method="POST" action="/profitHata">
+			<ul>
+				<li><input name="fullPrice" type="text" placeholder="Цена квартиры"></li>
+				<li><input name="firstPay" type="text" placeholder="Первый взнос"></li>
+				<li><input name="percent" type="text" placeholder="Процент по ипотеке"></li>
+				<li><input name="term" type="text" placeholder="Срок ипотеки"></li>
+			</ul>
+		</form>
+	</div>
+	<h1>Результаты расчёта</h1>
+	<div class="result">
+	</div>
 </body>
