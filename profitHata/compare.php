d69ea5dd542d1dff39f338cdb72b2261c9c7f9a5
ladiwals <vladjeslav@gmail.com>
2021-02-27 20:41:36 +0300

    Ипотека/Автоматизировал вывод формы и расчёты в сравнении, и настроли стили для нормального отображения

--- profitHata/compare.php
+++ profitHata/compare.php
@@ -1,24 +1,28 @@
 <?php
 
+include_once($_SERVER['DOCUMENT_ROOT'] . '/profitHata/formData.php');
+
 // Когда пришла форма
 if (isset($_POST['submit'])) {
 	extract($_POST, EXTR_PREFIX_ALL, 'pst');
 
-	$credit = (int) $pst_fullPrice - (int) $pst_firstPay; // Сумма кредита
-	$monthTerm = (int) $pst_term * 12; // Срок кредита в месяцах
-	$monthRate = (int) $pst_percent / 12 / 100; // Ежемесячная ставка
-	$generalRate = (1 + $monthRate) ** $monthTerm; // Общая ставка
-	$monthPay = ($credit * $monthRate * $generalRate) / ($generalRate - 1); // Ежемесячный платёж
-	$yearPay = $monthPay * 12; // Годовая выплата
-	$fullPay = $yearPay * (int) $pst_term; // Общая выплата
-	$overPay = $fullPay - ((int) $pst_fullPrice - (int) $pst_firstPay); // Общая переплата по кредиту
-	$overPayPercent = $overPay / $fullPay * 100; // Процент переплаты
-	$firstPayPercent = $pst_firstPay / $pst_fullPrice * 100;
+	for($i = 1; $i <= 2; $i++) {
+		${'credit_' . $i} = (int) ${'pst_fullPrice_' . $i} - (int) ${'pst_firstPay_' . $i}; // Сумма кредита
+		${'monthTerm_' . $i} = (int) ${'pst_term_' . $i} * 12; // Срок кредита в месяцах
+		${'monthRate_' . $i} = (int) ${'pst_percent_' . $i} / 12 / 100; // Ежемесячная ставка
+		${'generalRate_' . $i} = (1 + ${'monthRate_' . $i}) ** ${'monthTerm_' . $i}; // Общая ставка
+		${'monthPay_' . $i} = (${'credit_' . $i} * ${'monthRate_' . $i} * ${'generalRate_' . $i}) / (${'generalRate_' . $i} - 1); // Ежемесячный платёж
+		${'yearPay_' . $i} = ${'monthPay_' . $i} * 12; // Годовая выплата
+		${'fullPay_' . $i} = ${'yearPay_' . $i} * (int) ${'pst_term_' . $i}; // Общая выплата
+		${'overPay_' . $i} = ${'fullPay_' . $i} - ((int) ${'pst_fullPrice_' . $i} - (int) ${'pst_firstPay_' . $i}); // Общая переплата по кредиту
+		${'overPayPercent_' . $i} = ${'overPay_' . $i} / ${'fullPay_' . $i} * 100; // Процент переплаты
+		${'firstPayPercent_' . $i} = ${'pst_firstPay_' . $i} / ${'pst_fullPrice_' . $i} * 100;
 
-	$minSalary = $monthPay + $pst_monthSpend; // Необходимая зарплата
-	$toFirstPayMonth = $pst_firstPay / ($pst_currentSalary- $pst_monthSpend - $pst_monthRent); // Сколько месяцев потребуется, чтобы накопить на первый взнос
+		${'minSalary_' . $i} = ${'monthPay_' . $i} + ${'pst_monthSpend_' . $i}; // Необходимая зарплата
+		${'toFirstPayMonth_' . $i} = ${'pst_firstPay_' . $i} / (${'pst_currentSalary_' . $i} - ${'pst_monthSpend_' . $i} - ${'pst_monthRent_' . $i}); // Сколько месяцев потребуется, чтобы накопить на первый взнос
 
-	$suffix = ((int) $pst_term % 10 == 1) ? 'год' : (in_array((int) $pst_term % 10, [2, 3, 4]) ? 'года' : 'лет');
+		${'suffix_' . $i} = ((int) ${'pst_term_' . $i} % 10 == 1) ? 'год' : (in_array((int) ${'pst_term_' . $i} % 10, [2, 3, 4]) ? 'года' : 'лет');
+	}
 }
 
 function explodeThousand($number) 
@@ -48,92 +52,32 @@ function explodeThousand($number)
 <body>
 	<h1>Заполните все данные и нажмите "рассчитать"</h1>
 
-	<div class="compare">
+	<center>
+		<div class="compare">
 
-		<form method="POST" action="/profitHata/advanced.php">
+		<form method="POST" action="/profitHata/compare.php">
 			<table>
 
-				<tr>
-					<td>
-						<div class="hint">Цена квартиры-1</div>
-						<input name="fullPrice" type="text" value="<?=isset($pst_fullPrice) ? $pst_fullPrice : ''?>">
-					</td>
-					<td>
-						<div class="hint">Цена квартиры-2</div>
-						<input name="fullPrice" type="text" value="<?=isset($pst_fullPrice) ? $pst_fullPrice : ''?>">
-					</td>
-				</tr>
-
-				<tr>
-					<td>
-				<div class="hint">Первый взнос-1</div>
-				<input name="firstPay" type="text" value="<?=isset($pst_firstPay) ? $pst_firstPay : ''?>">
-					</td>
-					<td>
-				<div class="hint">Первый взнос-2</div>
-				<input name="firstPay" type="text" value="<?=isset($pst_firstPay) ? $pst_firstPay : ''?>">
-					</td>
-				</tr>
-
-				<tr>
-					<td>
-				<div class="hint">Процент по ипотеке-1</div>
-				<input name="percent" type="text" value="<?=isset($pst_percent) ? $pst_percent : ''?>">
-					</td>
-					<td>
-						<div class="hint">Процент по ипотеке-2</div>
-						<input name="percent" type="text" value="<?=isset($pst_percent) ? $pst_percent : ''?>">
-					</td>
-				</tr>
-				
-				<tr>
-					<td>
-				<div class="hint">Срок ипотеки в годах-1</div>
-				<input name="term" type="text" value="<?=isset($pst_term) ? $pst_term : ''?>">
-					</td>
-					<td>
-						<div class="hint">Срок ипотеки в годах-2</div>
-						<input name="term" type="text" value="<?=isset($pst_term) ? $pst_term : ''?>">
-					</td>
-				</tr>
-				
-				<tr>
-					<td>
-				<div class="hint">Ежемесячные траты на жизнь-1</div>
-				<input name="monthSpend" type="text" value="<?=isset($pst_monthSpend) ? $pst_monthSpend : ''?>">
-					</td>
-					<td>
-						<div class="hint">Ежемесячные траты на жизнь-2</div>
-						<input name="monthSpend" type="text" value="<?=isset($pst_monthSpend) ? $pst_monthSpend : ''?>">
-					</td>
-				</tr>
-				
-				<tr>
-					<td>
-				<div class="hint">Ежемесячные траты на съём до ипотеки-1</div>
-				<input name="monthRent" type="text" value="<?=isset($pst_monthRent) ? $pst_monthRent : ''?>">
-					</td>
-					<td>
-				<div class="hint">Ежемесячные траты на съём до ипотеки-2</div>
-				<input name="monthRent" type="text" value="<?=isset($pst_monthRent) ? $pst_monthRent : ''?>">
-					</td>
-				</tr>
-
-				<tr>
-					<td>
-				<div class="hint">Текущая зарплата-1</div>
-				<input name="currentSalary" type="text" value="<?=isset($pst_currentSalary) ? $pst_currentSalary : ''?>">
-					</td>
-					<td>
-				<div class="hint">Текущая зарплата-2</div>
-				<input name="currentSalary" type="text" value="<?=isset($pst_currentSalary) ? $pst_currentSalary : ''?>">
-					</td>
+				<?php
+				 foreach($formList as $item) { ?>
+				 	<tr>
+				 		<td>
+				 			<div class="hint"><?=$item['label'] . ' 1'?></div>
+				 			<input name="<?=$item['name'] . '_1'?>" type="text" value="<?=isset(${'pst_' . $item['name'] . '_1'}) ? ${'pst_' . $item['name'] . '_1'} : ''?>">
+				 		</td>
+				 		<td>
+				 			<div class="hint"><?=$item['label'] . '2'?></div>
+				 			<input name="<?=$item['name'] . '_2'?>" type="text" value="<?=isset(${'pst_' . $item['name'] . '_2'}) ? ${'pst_' . $item['name'] . '_2'} : ''?>">
+				 		</td>
+				 	</tr>
+				 <?php } ?>
 
 			</table>
 			<input type="submit" name="submit" value="Рассчитать">
 		</form>
 
-	</div>
+		</div>
+	</center>
 
 	<?php
 	if (isset($_POST['submit'])) {
---
+++ profitHata/formData.php
@@ -0,0 +1,32 @@
+<?php
+
+$formList = [
+	[
+		'label' => 'Цена квартиры',
+		'name' => 'fullPrice'
+	],
+	[
+		'label' => 'Первый взнос', 
+		'name' => 'firstPay'
+	],
+	[
+		'label' => 'Процент по ипотеке',
+		'name' => 'percent'
+	],
+	[
+		'label' => 'Срок ипотеки в годах',
+		'name' => 'term'
+	],
+	[
+		'label' => 'Ежемесячные траты на жизнь',
+		'name' => 'monthSpend'
+	],
+	[
+		'label' => 'Ежемесячные траты на съём до ипотеки',
+		'name' => 'monthRent'
+	],
+	[
+		'label' => 'Текущая зарплата',
+		'name' => 'currentSalary'
+	],
+];
